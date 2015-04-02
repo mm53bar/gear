f20416e9d691d11da3e3cae30c43f8d7458fa5f1
Michael McClenaghan <mike@sideline.ca>
2015-04-02 16:51:21 +0000

    Added a few more records to the fixtures

--- test/repositories/item_repository_test.rb
+++ test/repositories/item_repository_test.rb
@@ -7,12 +7,12 @@ class ItemRepositoryTest < ActiveSupport::TestCase
   
   def test_retrieving_all_only_retrieves_active_items
     backpack = items(:circuit_backpack) 
-    assert_equal 2, @repo.all.count
+    assert_equal 3, @repo.all.count
     assert_nil backpack.deleted_at
     @repo.destroy(backpack)
     
     backpack = @repo.find(backpack.id)
     refute_nil backpack.deleted_at
-    assert_equal 1, @repo.all.count
+    assert_equal 2, @repo.all.count
   end
 end
